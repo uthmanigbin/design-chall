07fd7cffc14e60923b32c635fc5efb528fab62ec
Uthman <uthmanigbin@gmail.com>
2024-05-22 22:54:22 +0300

    SVG download
    Little lesson on SVG retrieval

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import BackgroundTogether from "@/components/page";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <BackgroundTogether />
+    </div>
+  );
+}
---
+++ components/page.tsx
@@ -0,0 +1,9 @@
+const BackgroundTogether = () => {
+    return ( 
+        <div className="w-9 bg-cyan-300 ">
+            Hi
+        </div>
+     );
+}
+ 
+export default BackgroundTogether;
